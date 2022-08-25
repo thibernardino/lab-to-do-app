023c9cdc6ad53b5bd8e683f0966e84c4b4dc225a
thibernardino <Lenovo3,1>
2022-08-26 01:29:13 +0200

    fixing errors

--- src/main.js
+++ src/main.js
@@ -10,10 +10,3 @@ app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(faSpinner)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-new Vue({  router,  store,  render: h => h(App),}).$mount('#app')
